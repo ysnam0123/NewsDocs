1715cdeb225cb9066c1346bd28063828f038bd61
Bomin Kim <sirubom1203@gmail.com>
2025-06-02 15:18:09 +0900

    🎉 Init: 프로젝트 초기 생성

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <h1>App</h1>
+</template>
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <h1>AboutView</h1>
+</template>
+<style scoped></style>
