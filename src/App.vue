348889425d72292eee747d7332f219ae7bc27087
snowari <pes1149@naver.com>
2025-06-04 16:40:29 +0900

    ✨ Feat : login router 설정

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 <template>
-  <h1>App</h1>
+  <router-view />
 </template>
 <style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'LoginView',
+  data() {
+    return {}
+  },
+}
+</script>
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-[#ECE1FF]">
+    <div class="rounded-[20px] w-[770px] h-[920px]">
+      <h1>NewsDocs</h1>
+    </div>
+  </div>
+</template>
+<style scoped></style>
