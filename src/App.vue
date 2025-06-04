87cf5d9bc34657ea0349ea2422227e2dc7390568
kang-min-ryu <dbrkdalswkd@gmail.com>
2025-06-04 16:17:33 +0900

    NewsComponet 생성

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import NewsComponent1 from '@/components/NewsComponents/NewsComponent1.vue'
+import NewsComponent2 from '@/components/NewsComponents/NewsComponent2.vue'
+import NewsComponent3 from '@/components/NewsComponents/NewsComponent3.vue'
+import NewsComponent4 from '@/components/NewsComponents/NewsComponent4.vue'
+import NewsComponent5 from '@/components/NewsComponents/NewsComponent5.vue'
+</script>
 <template>
-  <h1>App</h1>
+  <div class="m-2 flex flex-col gap-y-4">
+    <NewsComponent1 />
+    <NewsComponent2 />
+    <NewsComponent3 />
+    <NewsComponent4 />
+    <NewsComponent5 />
+  </div>
 </template>
 <style scoped></style>
