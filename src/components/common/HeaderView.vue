3efca61b4b356e6c7be772bbf2cafb360b186c4f
namyoonseo <ysnam0123@naver.com>
2025-06-06 11:52:44 +0900

    🚨 Fix:헤더 로고 폰트 해제, tailwind CSS로 변경

--- src/components/common/HeaderView.vue
+++ src/components/common/HeaderView.vue
@@ -2,123 +2,50 @@
 import ButtonVue from './ButtonView.vue'
 </script>
 <template>
-  <div class="header">
-    <div class="languageBox">
-      <div class="language">한국어</div>
-      <div class="language">English</div>
-      <div class="language">中语</div>
-      <div class="language">日本語</div>
-    </div>
-    <div class="headerContainer">
-      <div class="logoBox">
+  <div class="px-[30px] py-[0px]">
+    <div class="px-[30px] py-[0px] flex items-center">
+      <div class="flex items-center gap-[10px] cursor-pointer">
         <img src="../../assets/icons/headerLogoImg.svg" alt="logoImg" />
-        <h1 class="logo">NewsDocs</h1>
+        <h1 class="text-[40px] select-none">NewsDocs</h1>
       </div>
-      <div class="headerNav">
-        <ul>
-          <li>맞춤 소식</li>
-          <li>다른 소식</li>
-          <li>의견 나누기</li>
+      <div class="ml-[30px]">
+        <ul class="flex gap-[15px] seelct-none mt-[10px]">
+          <li
+            class="cursor-pointer w-[120px] px-[12px] py-[8px] ease-in duration-75 hover:text-[#7537e3] hover:font-bold text-[18px]"
+          >
+            맞춤 소식
+          </li>
+          <li
+            class="cursor-pointer w-[120px] px-[12px] py-[8px] ease-in duration-75 hover:text-[#7537e3] hover:font-bold text-[18px]"
+          >
+            다른 소식
+          </li>
+          <li
+            class="cursor-pointer w-[120px] px-[12px] py-[8px] ease-in duration-75 hover:text-[#7537e3] hover:font-bold text-[18px]"
+          >
+            의견 나누기
+          </li>
         </ul>
       </div>
-      <div class="headerButtonBox">
-        <div class="search">
-          <input type="text" class="searchBox" placeholder="키워드로 뉴스 검색" />
-          <img src="../../assets/icons/searchIcon.svg" alt="searchIcon" class="searchIcon" />
+      <div class="flex gap-[10px] ml-auto">
+        <div class="ml-[40px] relative">
+          <input
+            type="text"
+            class="w-[300px] h-[40px] px-[10px] py-[5px] border-1 border-[#939393] rounded-xl"
+            placeholder="키워드로 뉴스 검색"
+          />
+          <img
+            src="../../assets/icons/searchIcon.svg"
+            alt="searchIcon"
+            class="absolute right-[10px] top-[8px] cursor-pointer"
+          />
         </div>
-        <ButtonVue class="logInBtn">로그인</ButtonVue>
+        <ButtonVue
+          class="w-[100px] h-[40px] px-[10px] py-[5px] ease-in duration-75 hover:bg-[#7956bf] active:scale-[0.95]"
+          >로그인</ButtonVue
+        >
       </div>
     </div>
   </div>
 </template>
-<style scoped>
-.header {
-  padding: 0 100px;
-  height: 100px;
-}
-.languageBox {
-  display: flex;
-  gap: 10px;
-  justify-content: end;
-  margin-right: 35px;
-}
-.language {
-  text-decoration: underline;
-  cursor: pointer;
-  color: #989898;
-}
-.headerContainer {
-  padding: 0px 30px;
-  display: flex;
-  align-items: center;
-}
-.logoBox {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  cursor: pointer;
-}
-.logo {
-  font-size: 40px;
-  user-select: none;
-  font-family: 'BagelFatOne', BagelFatOne, sans-serif;
-}
-.headerNav {
-  margin-left: 30px;
-}
-.headerNav ul {
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  user-select: none;
-  margin-top: 10px;
-}
-.headerNav ul li {
-  cursor: pointer;
-  width: 120px;
-  padding: 8px 12px;
-  transition: all 0.3s ease;
-  border-radius: 10px;
-  font-weight: 700;
-  text-align: center;
-}
-.headerNav ul li:hover {
-  color: #7537e3;
-  font-weight: 700;
-  font-size: 18px;
-}
-.search {
-  margin-left: 40px;
-  position: relative;
-}
-.searchBox {
-  width: 300px;
-  height: 40px;
-  padding: 5px 10px;
-  border: 1px solid #939393;
-  border-radius: 10px;
-}
-.searchIcon {
-  position: absolute;
-  right: 10px;
-  top: 8px;
-  cursor: pointer;
-}
-.headerButtonBox {
-  display: flex;
-  gap: 10px;
-  margin-left: auto;
-}
-.logInBtn {
-  width: 100px;
-  height: 40px;
-  padding: 5px 10px;
-  transition: all 0.1s ease;
-}
-.logInBtn:hover {
-  background-color: #7956bf;
-}
-.logInBtn:active {
-  transform: scale(0.95);
-}
-</style>
+<style scoped></style>
