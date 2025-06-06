4445ac0acabb98e8bc8e8ee0f724820e8278d12b
namyoonseo <ysnam0123@naver.com>
2025-06-06 15:47:31 +0900

    icon 폴더 추가

---
+++ src/components/icon/profileDog.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+<<<<<<<< HEAD:src/components/common/FooterVue.vue
+  <h1>footer</h1>
+========
+  <img src="@/assets/img/logoDog.svg" />
+>>>>>>>> dev:src/components/common/profileDog.vue
+</template>
+<style scoped></style>
---
+++ src/components/icon/sleepDog.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <img src="@/assets/img/sleepDog.svg" />
+</template>
+<style scoped></style>
