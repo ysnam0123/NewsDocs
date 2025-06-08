611ad05ae7c1c4f30b7fa03d9bf86cf34ecd7c9f
nam yoonseo <158164219+ysnam0123@users.noreply.github.com>
2025-06-08 23:09:58 +0900

    Revert "✨ Feat: 나의 관심뉴스 페이지 퍼블리싱"

--- src/components/icon/blackBtn.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-<template>
-  <button class="cursor-pointer">
-    <img src="@/assets/img/blackBtn.svg" />
-  </button>
-</template>
-<style scoped></style>
--- src/components/icon/thumbsUpOpacity.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-<template>
-  <button class="cursor-pointer">
-    <img src="@/assets/img/Thumbs-up-opacity.svg" class="w-3.5 h-3.5" />
-  </button>
-</template>
-<style scoped></style>
