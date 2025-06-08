bb5075c7d87261e31cde72c1ff8907d27f403240
namyoonseo <ysnam0123@naver.com>
2025-06-08 23:15:25 +0900

    헤더 수정, 뉴스컴포넌트 퍼블리싱 중

--- src/components/common/HeaderView.vue
+++ src/components/common/HeaderView.vue
@@ -1,123 +1,65 @@
 <script setup>
-import ButtonVue from './ButtonVue.vue'
 import logo from '@/assets/img/logo_newsdocs.png'
 </script>
 <template>
-  <div class="header">
-    <div class="languageBox">
-      <div class="language">한국어</div>
-      <div class="language">English</div>
-      <div class="language">中语</div>
-      <div class="language">日本語</div>
+  <div class="w-[1240px] mx-auto py-[0px] flex items-center h-[100px]">
+    <div class="flex items-center gap-[10px] cursor-pointer">
+      <img :src="logo" alt="logoImg" class="w-[202px] h-11.5" />
     </div>
-    <div class="headerContainer">
-      <div class="logoBox">
-        <img :src="logo" alt="logoImg" class="w-[202px] h-11.5" />
-        <h1 class="logo"></h1>
+    <div class="ml-[30px]">
+      <ul class="flex gap-[7px] select-none">
+        <li
+          class="cursor-pointer w-[120px] px-[12px] py-[8px] ease-in duration-75 hover:text-[#7537e3] hover:font-bold text-[18px]"
+        >
+          맞춤 소식
+        </li>
+        <li
+          class="cursor-pointer w-[120px] px-[12px] py-[8px] ease-in duration-75 hover:text-[#7537e3] hover:font-bold text-[18px]"
+        >
+          다른 소식
+        </li>
+        <li
+          class="cursor-pointer w-[120px] px-[12px] py-[8px] ease-in duration-75 hover:text-[#7537e3] hover:font-bold text-[18px]"
+        >
+          의견 나누기
+        </li>
+      </ul>
+    </div>
+    <div class="flex items-center gap-[10px] ml-auto">
+      <div class="ml-[40px] relative">
+        <input
+          type="text"
+          class="w-[300px] h-[40px] px-[10px] py-[5px] border-1 border-[#939393] rounded-xl"
+          placeholder="키워드로 뉴스 검색"
+        />
+        <img
+          src="../../assets/icons/searchIcon.svg"
+          alt="searchIcon"
+          class="absolute right-[10px] top-[8px] cursor-pointer"
+        />
       </div>
-      <div class="headerNav">
-        <ul>
-          <li>맞춤 소식</li>
-          <li>다른 소식</li>
-          <li>의견 나누기</li>
-        </ul>
+      <div class="cursor-pointer">
+        <img
+          src="@/assets/icons/changeInterest.svg"
+          alt="changeInterest"
+          class="w-[96px] h-[41px]"
+        />
       </div>
-      <div class="headerButtonBox">
-        <div class="search">
-          <input type="text" class="searchBox" placeholder="키워드로 뉴스 검색" />
-          <img src="../../assets/icons/searchIcon.svg" alt="searchIcon" class="searchIcon" />
-        </div>
-        <ButtonVue class="logInBtn">로그인</ButtonVue>
+      <div
+        class="w-[40px] h-[40px] cursor-pointer rounded-[100%] bg-[#F6F6F6] flex items-center justify-center"
+      >
+        <img src="@/assets/icons/toDarkMode.svg" alt="modeToggle" class="w-[24px] h-[24px]" />
+      </div>
+      <div
+        class="w-[40px] h-[40px] cursor-pointer rounded-[100%] bg-[#F6F6F6] flex items-center justify-center"
+      >
+        <img src="@/assets/icons/notification.svg" alt="notification" class="w-[24px] h-[24px]" />
+      </div>
+      <div
+        class="w-[40px] h-[40px] cursor-pointer rounded-[100%] bg-[#F6F6F6] flex items-center justify-center"
+      >
+        <img src="@/assets/icons/profile.svg" alt="profile" class="w-[24px] h-[24px]" />
       </div>
     </div>
   </div>
 </template>
-<style scoped>
-.header {
-  padding: 0 100px;
-  height: 100px;
-}
-.languageBox {
-  display: flex;
-  gap: 10px;
-  justify-content: end;
-  margin-right: 35px;
-}
-.language {
-  text-decoration: underline;
-  cursor: pointer;
-  color: #989898;
-}
-.headerContainer {
-  padding: 0px 30px;
-  display: flex;
-  align-items: center;
-}
-.logoBox {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  cursor: pointer;
-}
-.logo {
-  font-size: 40px;
-  user-select: none;
-  font-family: 'Luckiest Guy', BagelFatOne, sans-serif;
-}
-.headerNav {
-  margin-left: 30px;
-}
-.headerNav ul {
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  user-select: none;
-}
-.headerNav ul li {
-  cursor: pointer;
-  width: 120px;
-  padding: 8px 12px;
-  transition: all 0.3s ease;
-  border-radius: 10px;
-  text-align: center;
-}
-.headerNav ul li:hover {
-  color: #7537e3;
-  font-weight: 700;
-  font-size: 18px;
-}
-.search {
-  margin-left: 40px;
-  position: relative;
-}
-.searchBox {
-  width: 300px;
-  height: 40px;
-  padding: 5px;
-  border: 1px solid #939393;
-  border-radius: 10px;
-}
-.searchIcon {
-  position: absolute;
-  right: 10px;
-  top: 8px;
-  cursor: pointer;
-}
-.headerButtonBox {
-  display: flex;
-  gap: 10px;
-  margin-left: auto;
-}
-.logInBtn {
-  width: 100px;
-  height: 40px;
-  padding: 5px 10px;
-  transition: all 0.1s ease;
-}
-.logInBtn:hover {
-  background-color: #7956bf;
-}
-.logInBtn:active {
-  transform: scale(0.95);
-}
-</style>
