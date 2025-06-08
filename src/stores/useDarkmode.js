5204776c11af7fe2da42cfbe98da8b7b0cec4778
Bomin Kim <sirubom1203@gmail.com>
2025-06-08 21:13:25 +0900

    ✨ Feat: 다크모드 설정

---
+++ src/stores/useDarkmode.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+import { useDark, useToggle } from '@vueuse/core'
+
+export const useThemeStore = defineStore('theme', () => {
+  const isDark = useDark({
+    selector: 'html',
+    attribute: 'class',
+    valueDark: 'dark',
+    valueLight: '',
+  })
+  const toggleDark = useToggle(isDark)
+
+  return { isDark, toggleDark }
+})
