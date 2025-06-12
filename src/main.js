59bb21186f84627af9ea86039653c22cb77021d2
snowari <70631889+snowari@users.noreply.github.com>
2025-06-12 12:30:43 +0900

    🚑 hotfix : pinia 연동 동기화

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import router from './router'
 import Toast from 'vue-toastification'
 // import '@/assets/toast-custom.css'
 import 'vue-toastification/dist/index.css'
+import supabase from './utils/supabase'
+import { userAuthStore } from './stores/authStore'
 
 const app = createApp(App)
 
@@ -22,6 +24,16 @@ app.use(Toast, {
   pauseOnHover: true,
   hideProgressBar: true,
   maxToasts: 1,
+})
+
+//토큰 상태 변화 감지 -> 피니아 반영
+supabase.auth.onAuthStateChange(async (event, session) => {
+  const authStore = userAuthStore()
+  if (session?.user) {
+    await authStore.fetchUser()
+  } else {
+    authStore.logout()
+  }
 })
 
 app.mount('#app')
