20a4faa48ab3891fccbb6c978a40f2c8230b3f6b
snowari <70631889+snowari@users.noreply.github.com>
2025-06-17 10:45:45 +0900

    ✨ feat : sb에 저장할 때, 카테고리 맵핑해서 저장

---
+++ src/composables/useCategoryMap.js
@@ -0,0 +1,32 @@
+// utils/categoryMap.js
+export const categoryNameMap = {
+  politics: '정치',
+  sports: '스포츠',
+  business: '경제',
+  entertainment: '연예',
+  entertain: '연예',
+  culture: '문화',
+  abroad: '해외',
+  world: '해외',
+  society: '사회',
+  economy: '경제',
+  technology: '기술',
+  top: '사회',
+  other: '그 외',
+  etc: '그 외',
+}
+
+export const categoryIdMap = {
+  politics: 1, // 정치
+  sports: 2, // 스포츠
+  entertainment: 3, // 연예
+  entertain: 3, // 연예 (동의어)
+  culture: 4, // 문화
+  abroad: 5, // 해외
+  world: 5, // 해외 (동의어)
+  society: 6, // 사회
+  economy: 7, // 경제
+  business: 7, // 경제 (동의어)
+  etc: 8, // 그 외
+  other: 8, // 그 외 (동의어)
+}
