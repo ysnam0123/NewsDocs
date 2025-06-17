5562caaf23a7ce2695efcb3b9a3ea4ab7b67f0fc
Bomin Kim <sirubom1203@gmail.com>
2025-06-17 23:42:46 +0900

    임시 저장

--- src/components/NewsComponents/NewsComponent0.vue
+++ src/components/NewsComponents/NewsComponent0.vue
@@ -3,56 +3,50 @@ import { onMounted, ref } from 'vue'
 import ScrapImg from './children/ScrapImg.vue'
 import { ThumbsUp, Eye } from 'lucide-vue-next'
 import dogNotFound from '@/assets/img/dog-notfound-v2.png'
-import { useNewsActions } from '@/composables/useNewsActions'
 import { useSummary } from '@/composables/useSummary'
 import { useTyping } from '@/composables/useTyping'
-import { useSummaryStore } from '@/stores/summaryNews2'
 
-const { toDetailHandler } = useNewsActions()
-const { summarizeToggle } = useSummary()
+const { getOrCreateSummary } = useSummary()
 const { typedTarget, runTyped } = useTyping()
-const summaryStore = useSummaryStore()
 const summaryMessage = ref('')
-const isSummaryLoading = ref(true)
+const isLoading = ref(true)
 const isOpen = ref(false)
+const summary = ref('')
 const props = defineProps({
   news: Object,
 })
 
-const handleClick = async () => {
+const handleSummary = async () => {
   isOpen.value = !isOpen.value
-  if (!isOpen.value) return
 
-  isSummaryLoading.value = true
-  summaryMessage.value = ''
+  if (summary.value) return
 
-  if (!props.news.description) {
-    summaryMessage.value = '이 뉴스는 원문에서 확인해주세요!'
-    return
+  isLoading.value = true
+
+  const result = await getOrCreateSummary(props.news.article_id, props.news.description)
+  if (result) {
+    summary.value = result
+    await runTyped(result)
   }
 
-  await summarizeToggle(props.news.article_id, props.news.description)
-  await runTyped(summaryStore.getSummary(props.news.article_id))
-
-  isSummaryLoading.value = false
+  isLoading.value = false
 }
 
-// 마운트 후 상태 초기화
 onMounted(() => {
   if (props.news) {
-    console.log('🟢 컴포넌트 Mounted')
+    console.log('0번 컴포넌트 Mounted')
   }
 })
 </script>
 <template>
   <div
     v-if="props.news"
-    class="relative rounded-[16px] w-[786px] h-[468px] select-none group"
-    @click.stop="handleClick"
+    class="relative rounded-[16px] h-[468px] select-none group"
+    @click.stop="handleSummary"
   >
     <!-- 호버했을때 나오는 창 -->
     <div
-      class="absolute h-[468px] hover:inset-0 hover:bg-black/30 rounded-[20px] flex items-center justify-center text-center z-12 cursor-pointer"
+      class="absolute w-full h-full hover:inset-0 hover:bg-black/30 rounded-[20px] flex items-center justify-center text-center z-12 cursor-pointer"
     >
       <p class="hidden group-hover:flex text-white text-[16px] hover:z-20">요약보기</p>
     </div>
@@ -61,12 +55,11 @@ onMounted(() => {
       class="absolute cursor-pointer inset-0 bg-black/70 hover:bg-black/80 flex flex-col items-center gap-4 rounded-[16px] z-20 backdrop-blur-lg"
     >
       <!-- 클릭했을 때 나오는 창 -->
-      <template v-if="isSummaryLoading">
+      <template v-if="isLoading">
         <div class="w-full flex flex-col py-10 px-7 animate-pulse shrink-0">
-          <div class="mb-6 h-7 w-[100px] bg-[#626262]/70 rounded-md"></div>
+          <div class="mb-6 h-9 w-[100px] bg-[#626262]/70 rounded-md"></div>
           <div class="mb-4 h-8 w-[530px] bg-[#626262]/70 rounded-md"></div>
           <div class="mb-4 h-8 w-[510px] bg-[#626262]/70 rounded-md"></div>
-          <div class="mb-4 h-8 w-[460px] bg-[#626262]/70 rounded-md"></div>
           <div class="h-8 w-[400px] bg-[#626262]/70 rounded-md"></div>
         </div>
       </template>
@@ -80,30 +73,30 @@ onMounted(() => {
         </div>
       </template>
 
-      <div v-show="!isSummaryLoading && !summaryMessage">
-        <div class="w-[700px] h-[468px] rounded-[20px] top-0 pt-[40px] pb-[32px] px-[30px]">
-          <div class="flex flex-col z-30 h-full">
-            <h1 class="text-[20px] font-semibold text-white mb-[24px]">세 줄 요약</h1>
-            <div class="max-h-[220px] pr-1">
-              <div class="text-white whitespace-pre-line leading-8">
-                <span ref="typedTarget" class="text-white text-[20px]"></span>
+      <div v-show="!isLoading">
+        <div class="w-full h-[468px] rounded-[20px] pt-[40px] pb-[32px] px-[30px]">
+          <div class="flex flex-col z-30 h-full text-white">
+            <h1 class="text-[20px] font-semibold mb-[24px]">세 줄 요약</h1>
+            <div class="max-h-[220px] pr-1 w-full">
+              <div class="whitespace-pre-line leading-8 w-full text-left">
+                <span ref="typedTarget" class="text-[20px] w-full text-left"></span>
               </div>
             </div>
           </div>
         </div>
-        <button
+        <router-link
+          :to="`/news/detail/${props.news.article_id}`"
           class="absolute bottom-5 right-4 z-30 w-[81px] h-[33px] px-[16px] py-[8px] text-[14px] font-semibold bg-white rounded-[8px] mt-[16px] ml-auto flex items-center cursor-pointer hover:bg-[#D2D2D2]"
-          @click="toDetailHandler(props.news)"
         >
           원문보기
-        </button>
+        </router-link>
       </div>
     </div>
 
     <img :src="news.image_url" alt="slide" class="w-full h-full object-cover rounded-[16px]" />
     <div
       v-if="props.news.description"
-      class="flex flex-col gap-2 px-[20px] absolute w-[786px] h-[468px] bg-linear-to-t from-black to-transparent bottom-0 z-10 rounded-[16px]"
+      class="flex flex-col gap-2 px-[20px] absolute w-full h-[468px] bg-linear-to-t from-black to-transparent bottom-0 z-10 rounded-[16px]"
     >
       <p class="text-[20px] text-white mt-auto break-words line-clamp-2">
         {{ props.news.title }}
--- src/composables/useSummary.js
+++ src/composables/useSummary.js
@@ -1,10 +1,7 @@
 import supabase from '@/utils/supabase'
-import { useSummaryStore } from '@/stores/summaryNews2'
 import { fetchOpenAi } from '@/api/fetchOpenAi'
 
 export const useSummary = () => {
-  const summaryStore = useSummaryStore()
-
   // 클릭하면 뉴스 정보로 요약 불러오기, 없으면 생성
   const getOrCreateSummary = async (articleId, description) => {
     try {
@@ -21,15 +18,12 @@ export const useSummary = () => {
         return null
       }
       if (savedSummary?.summaries_contents) {
-        summaryStore.setSummary(articleId, savedSummary.summaries_contents)
         console.log('기존 요약')
         return savedSummary.summaries_contents
       }
       // open ai 요약 생성
       const result = await fetchOpenAi(description)
-      summaryStore.setSummary(articleId, result)
       console.log('요약 결과:', result)
-      console.log('저장된 요약:', summaryStore.getSummary(articleId))
       const { data: existingNews } = await supabase
         .from('news')
         .select('news_id')
@@ -40,7 +34,7 @@ export const useSummary = () => {
         console.warn('news테이블에 해당 뉴스가 존재하지 않음 요약 저장 실패')
         return result
       }
-      // summary insert
+      // 새로 만든 summary insert
       const { error: insertError } = await supabase
         .from('summaries')
         .insert([{ news_id: articleId, summaries_contents: result }])
@@ -54,10 +48,7 @@ export const useSummary = () => {
     }
   }
   const summarizeToggle = async (articleId, description) => {
-    const existing = summaryStore.getSummary(articleId)
-    if (!existing) {
-      await getOrCreateSummary(articleId, description)
-    }
+    await getOrCreateSummary(articleId, description)
   }
 
   return {
