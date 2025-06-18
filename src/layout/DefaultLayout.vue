33ef847532879873b012626d9479e56017d6bea7
snowari <70631889+snowari@users.noreply.github.com>
2025-06-18 18:41:39 +0900

    💄 style : 반응형 및 다크모드 추가

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -2,13 +2,24 @@
 import HeaderView from '@/components/common/HeaderView.vue'
 import FooterView from '@/components/common/FooterView.vue'
 import { useRoute } from 'vue-router'
-import HeaderBasic from '@/components/common/HeaderBasic.vue'
+import { ref, computed } from 'vue'
+import HeaderMobileBasic from '@/components/common/HeaderMobileBasic.vue'
 
 const route = useRoute()
+const isMobile = ref(window.innerWidth < 640)
+window.addEventListener('resize', () => {
+  isMobile.value = window.innerWidth < 640
+})
+
+const showHeaderView = computed(() => !route.meta.hideHeader && !isMobile.value)
+const showHeaderBasic = computed(() => !route.meta.hideHeaderBasic && !isMobile.value)
+const showMobileHeader = computed(() => isMobile.value && !route.meta.hideHeader)
 </script>
 <template>
-  <HeaderView v-if="!route.meta.hideHeader" />
-  <HeaderBasic v-else-if="!route.meta.hideHeaderBasic" />
+  <HeaderView v-if="showHeaderView" />
+  <HeaderBasic v-else-if="showHeaderBasic" />
+  <!-- 모바일 헤더 -->
+  <HeaderMobileBasic v-if="showMobileHeader" />
   <div class="min-h-[100vh]">
     <slot></slot>
   </div>
